8c6f7ce0db58c16caa6ea7997e848597aeba01b5
Watson <kowalski@Watsons-MacBook-Pro.local>
2019-08-29 11:04:25 +0700

    bar volume

---
+++ app/src/main/java/com/belajar/samplebarvolume/MainActivity.java
@@ -0,0 +1,100 @@
+package com.belajar.samplebarvolume;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//    membuat variable berdasarkan layout
+    EditText edtHeight, edtLength, edtWidth;
+    Button btnCalculate;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//  memasukan id layout pada variable
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+//  set onclicklistener untuk tombol pada activity ini
+        btnCalculate.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_calculate) {
+//          menyimpan text kolom pada variable
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+//          Mengecek nilai kosong pada kolom
+            boolean isEmptyFields = false;
+            boolean isInvalidDouble = false;
+
+            if (TextUtils.isEmpty(inputWidth)) {
+                isEmptyFields = true;
+                edtWidth.setError("Masukkan nilai lebar!");
+            }
+
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true;
+                edtHeight.setError("Masukkan nilai tinggi!");
+            }
+
+            if (TextUtils.isEmpty(inputLength)) {
+                isEmptyFields = true;
+                edtLength.setError("Masukkan nilai panjang!");
+            }
+
+//          Mengecek nilai masukan angka
+            Double length = toDouble(inputLength);
+            Double width = toDouble(inputWidth);
+            Double height = toDouble(inputHeight);
+
+            if (length == null) {
+                isInvalidDouble = true;
+                edtLength.setError("Field ini harus berupa nomor yang valid!");
+            }
+
+            if (width == null) {
+                isInvalidDouble = true;
+                edtWidth.setError("Field ini harus berupa nomor yang valid!");
+            }
+
+            if (height == null) {
+                isInvalidDouble = true;
+                edtHeight.setError("Field ini harus berupa nomor yang valid!");
+            }
+
+            if (!isEmptyFields && !isInvalidDouble) {
+                double volume = length * width * height;
+
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+    }
+
+    Double toDouble(String str) {
+        try {
+            return Double.valueOf(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
